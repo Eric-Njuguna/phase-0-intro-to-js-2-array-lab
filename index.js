952878978092df2ee8702dfd87964b7ed4db18a8
Eric Brian Njuguna <eric.njuguna@student.moringaschool.com>
2024-06-19 06:27:15 +0300

    Adds functions which changes and resets arrays in index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function arrayFunctions(){
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+    return cats;
+ }
+
+ function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    let copyOfCats = [...cats,name];
+    return copyOfCats;
+}
+
+function prependCat(name){
+    let copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(){
+    let copyOfCats = [...cats];
+    copyOfCats.splice(-1);
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    let copyOfCats = [...cats];
+    copyOfCats.splice(0,1);
+    return copyOfCats;
+}
+
+console.log(arrayFunctions());
+//console.log(removeFirstCat());
